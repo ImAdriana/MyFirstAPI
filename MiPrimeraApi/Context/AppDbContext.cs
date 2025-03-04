27c346bd44b53b278e87664d9a9d492779a38004
Im Adriana Clemente <clemente.hernandez.alicia.adriana@gmail.com>
2025-03-03 19:51:11 -0600

    Project files

---
+++ MiPrimeraApi/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiPrimeraApi.Models;
+
+namespace MiPrimeraApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Person> Persons {  get; set; }
+    }
+}
+ 
